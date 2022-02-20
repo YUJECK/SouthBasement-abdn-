337bb4e10830a1c806fa974051958180ce341f92
YUJECK <kolajkuceravkin@gmail.com>
2022-02-20 12:14:16 +0300

    20.02.22
    Немного подправил атаку

--- Assets/Scripts/Attack/PointRotation.cs
+++ Assets/Scripts/Attack/PointRotation.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class PointRotation : MonoBehaviour
 {
-    public Vector2 mousePos;
+    public Vector3 mousePos;
     public Vector2 lookDir;
-    public Rigidbody2D rb;
-    public Rigidbody2D Player;
-    public Camera cam;
-    
+    public Transform Player;
+    public Transform Cursor;
     public float angle;
     public float stabilizator;
     public static PointRotation instance;
@@ -22,34 +20,29 @@ public class PointRotation : MonoBehaviour
             return;
         }
     }
-    void Start()
-    {
-        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-    }
-
     void FixedUpdate()
     {
-        rb.position = Player.position;
+        transform.position = Player.position;
         
         if(!GetComponent<RatAttack>().is_Attack)
         {
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            lookDir = mousePos - rb.position;
-            angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - stabilizator;
-            rb.rotation = angle;
+            lookDir = mousePos - transform.position;
+            angle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg - stabilizator;
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
         }
         else
-            rb.rotation = GetComponent<RatAttack>().sp_rotation;
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, GetComponent<RatAttack>().sp_rotation));
     }
 
     public void Flip()
     {
-        transform.Rotate(0f,180f,0f);
+        base.transform.Rotate(0f,180f,0f);
 
-        if(stabilizator == -118f)
-            stabilizator = -59f;
-        
-        else if(stabilizator == -59f) 
-            stabilizator = -118f;
+    // if(stabilizator == -118f)
+    //     stabilizator = -59f;
+    
+    // else if(stabilizator == -59f) 
+    //     stabilizator = -118f;
     }
 }
