73d37268feda2efc2b9cf31b90427ab66e3f5788
YUJECK <kolajkuceravkin@gmail.com>
2022-03-30 20:59:57 +0300

    30.03.22

--- Assets/Scripts/DecorationSprite.cs
+++ Assets/Scripts/DecorationSprite.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class DecorationSprite : MonoBehaviour
 {
     [SerializeField] private int layerPlus = 2;
+    [SerializeField] private bool changeLayer;
+    [SerializeField] private Collider2D _collider;
+    [SerializeField] private bool changeCollider;
 
     private void OnTriggerEnter2D(Collider2D coll)
     {
-        if(coll.tag == "Player")
+        if(coll.tag == "Player" && changeLayer)
             GetComponent<SpriteRenderer>().sortingOrder += layerPlus;
     }
     private void OnTriggerExit2D(Collider2D coll)
     {
-        if(coll.tag == "Player")
+        if(coll.tag == "Player"&& changeLayer)
             GetComponent<SpriteRenderer>().sortingOrder -= layerPlus;
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(changeCollider)
+        {
+            if(coll.gameObject.tag != "PlayerLegs")
+                _collider.enabled = false;
+            else if(coll.gameObject.tag == "PlayerLegs")
+                _collider.enabled = true;
+        }
     }
 }
