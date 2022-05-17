8ebe5aa0a40289f002a50638182833a30ad5caab
RimuruDev <Rimuru.Dev@gmail.com>
2022-05-17 18:42:38 +0400

    Added Rat Moving The Next Level
    Positioning on a new level.

---
+++ Assets/Scripts/RatCharacterController/Helpers/RatMovingTheNextLevel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+using RimuruDev.Mechanics.Character;
+using static RimuruDev.Helpers.Tag;
+
+namespace RimuruDev.Helpers
+{
+    public sealed class RatMovingTheNextLevel : MonoBehaviour
+    {
+        public RatCharacterData characterData;
+
+        private void Awake()
+        {
+            if (characterData == null)
+            {
+                characterData = GetComponent<RatCharacterData>();
+
+                if (characterData == null)
+                    characterData = GameObject.FindObjectOfType<RatCharacterData>();
+            }
+        }
+
+        private void OnLevelWasLoaded(int level) => SetPositionForNewLevel();
+
+        public void SetPositionForNewLevel()
+        {
+            characterData.ratCharacter.position =
+                GameObject.FindWithTag(StartPoint).transform.position;
+        }
+    }
+}
