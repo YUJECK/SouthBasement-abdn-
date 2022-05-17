d4c4ce2f28142ba9df0f6a5fcda47113656836dd
RimuruDev <Rimuru.Dev@gmail.com>
2022-05-17 18:38:14 +0400

    Added Rat Boost Speed class.
    Stores a method for speeding up a rat character.

---
+++ Assets/Scripts/RatCharacterController/Mechanics/RatBoostSpeed.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace RimuruDev.Mechanics.Character
+{
+    public sealed class RatBoostSpeed : MonoBehaviour
+    {
+        public RatCharacterData characterData;
+
+        private void Awake()
+        {
+            if (characterData == null)
+            {
+                characterData = GetComponent<RatCharacterData>();
+
+                if (characterData == null)
+                    characterData = GameObject.FindObjectOfType<RatCharacterData>();
+            }
+        }
+
+        public void BoostSpeed(float speedBoost) => characterData.speed += characterData.speed * speedBoost;
+    }
+}
