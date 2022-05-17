b0209da648705f35ea9dfb0c7578726aa133c3e3
RimuruDev <Rimuru.Dev@gmail.com>
2022-05-17 18:45:42 +0400

    Update RatSensei.cs

--- Assets/Scripts/NCPs/RatSensei.cs
+++ Assets/Scripts/NCPs/RatSensei.cs
@@ -1,17 +1,18 @@
+using RimuruDev.Mechanics.Character;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RatSensei : MonoBehaviour
 {
-    private Player player;
+    private RatCharacterData player;
     private RatAttack playerAttack;
     public int agility;
     public int strength;
 
     private void Start()
     {
-        player = FindObjectOfType<Player>();
+        player = FindObjectOfType<RatCharacterData>();
         playerAttack = FindObjectOfType<RatAttack>();
     }
 
