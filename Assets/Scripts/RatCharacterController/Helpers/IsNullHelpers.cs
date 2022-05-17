bdc4f4b0ea3530dd94abfdbd556a9c9c5fbc9666
RimuruDev <Rimuru.Dev@gmail.com>
2022-05-17 18:34:43 +0400

    Added component initialization helper.
    Beta version.

---
+++ Assets/Scripts/RatCharacterController/Helpers/IsNullHelpers.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace RimuruDev.Helpers
+{
+    public static class IsNullHelpers<T> where T : MonoBehaviour
+    {
+        public static T IsNullHelp(ref T checkRef, Component checkComponent)
+        {
+            if (checkRef == null)
+            {
+                checkRef = checkComponent.GetComponent<T>();
+
+                if (checkRef == null)
+                    return checkRef = GameObject.FindObjectOfType<T>();
+
+                return checkRef;
+            }
+
+            Debug.Log($"Type({checkRef})::Component({checkComponent}) == null.");
+
+            return default(T);
+        }
+    }
+}
