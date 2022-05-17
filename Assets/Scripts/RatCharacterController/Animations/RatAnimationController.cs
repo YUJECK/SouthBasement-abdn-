67265a296b45245eb7ba0f335661740c1bf13739
RimuruDev <Rimuru.Dev@gmail.com>
2022-05-17 18:43:39 +0400

    Added Rat Animation Controller.
    All rat character animations are here.

---
+++ Assets/Scripts/RatCharacterController/Animations/RatAnimationController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+using static RimuruDev.Helpers.Tag;
+
+namespace RimuruDev.Mechanics.Character
+{
+    public sealed class RatAnimationController : MonoBehaviour
+    {
+        public RatCharacterData characterData;
+        public RatMotionController motionController;
+
+        private void Awake()
+        {
+            if (characterData == null)
+            {
+                characterData = GetComponent<RatCharacterData>();
+
+                if (characterData == null)
+                    characterData = GameObject.FindObjectOfType<RatCharacterData>();
+            }
+
+            if (motionController == null)
+            {
+                motionController = GetComponent<RatMotionController>();
+
+                if (motionController == null)
+                    motionController = GameObject.FindObjectOfType<RatMotionController>();
+            }
+        }
+
+        public void RatRunAnimation()
+        {
+            if (motionController.GetPlayerInput().x != 0 || motionController.GetPlayerInput().y != 0)
+                characterData.anim.SetBool(Is_Run, true);
+
+            else if (motionController.GetPlayerInput().x == 0 && motionController.GetPlayerInput().y == 0)
+                characterData.anim.SetBool(Is_Run, false);
+        }
+
+        public void RatStayAnimation() => characterData.anim.SetBool(Is_Run, false);
+
+        public void RatRotate()
+        {
+            if (motionController.GetPlayerInput().x > 0)
+                characterData.ratSprite.flipX = true;
+
+            if (motionController.GetPlayerInput().x < 0)
+                characterData.ratSprite.flipX = false;
+        }
+    }
+}
