179da0ee9e13eee0c4e76cf138c18fcd0811d609
RimuruDev <Rimuru.Dev@gmail.com>
2022-05-17 18:41:57 +0400

    Added Rat Dashing Controller.
    The dash mechanics of the rat character.

---
+++ Assets/Scripts/RatCharacterController/Mechanics/RatDashingController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace RimuruDev.Mechanics.Character
+{
+    public sealed class RatDashingController : MonoBehaviour
+    {
+        public RatCharacterData characterData;
+        public RatMotionController motionController;
+
+        private void Awake()
+        {
+            if (characterData == null)
+            {
+                characterData = GetComponent<RatCharacterData>();
+
+                if (characterData == null)
+                    characterData = GameObject.FindObjectOfType<RatCharacterData>();
+            }
+
+            if (motionController == null)
+            {
+                motionController = GetComponent<RatMotionController>();
+
+                if (motionController == null)
+                    motionController = GameObject.FindObjectOfType<RatMotionController>();
+            }
+        }
+
+        public void GetDashingInput()
+        {
+            if (Input.GetMouseButtonDown(1) && !characterData.isSprinting && characterData.dashTime == 0f && characterData.dashNextTime <= Time.time)
+            {
+                characterData.dashTime = characterData.dashDuration;
+                characterData.movementOnDash = motionController.GetPlayerInput();
+                characterData.dashNextTime = Time.time + characterData.dashRate;
+            }
+        }
+
+        public bool IsGetDashingInput()
+        {
+            if (Input.GetMouseButtonDown(1) && !characterData.isSprinting && characterData.dashTime == 0f && characterData.dashNextTime <= Time.time)
+            {
+                characterData.dashTime = characterData.dashDuration;
+                characterData.movementOnDash = motionController.GetPlayerInput();
+                characterData.dashNextTime = Time.time + characterData.dashRate;
+
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void IsCallDashing()
+        {
+            if (characterData.dashTime > 0f)
+                Dashing();
+            else
+                characterData.dashTime = 0f;
+        }
+
+        public void Dashing()
+        {
+            characterData.ratRigidbody2D.velocity = characterData.movementOnDash * (characterData.speed + characterData.dashSpeed);
+            characterData.dashTime -= 0.1f;
+        }
+    }
+}
