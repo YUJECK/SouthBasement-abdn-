6ac42a9ac416538bf6cb61a4f249c81b91723379
RimuruDev <Rimuru.Dev@gmail.com>
2022-05-17 18:40:07 +0400

    Added Rat Motion Controller.
    The movement mechanics of the rat character.

---
+++ Assets/Scripts/RatCharacterController/Mechanics/RatMotionController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+using static RimuruDev.Helpers.Tag;
+
+namespace RimuruDev.Mechanics.Character
+{
+    public sealed class RatMotionController : MonoBehaviour
+    {
+        public RatCharacterData characterData;
+        private Vector2 movementDirection;
+
+        private void Awake()
+        {
+            if (characterData == null)
+            {
+                characterData = GetComponent<RatCharacterData>();
+
+                if (characterData == null)
+                    characterData = GameObject.FindObjectOfType<RatCharacterData>();
+            }
+        }
+
+        private void Update() => movementDirection = GetPlayerInput();
+
+        public Vector2 GetPlayerInput() => new Vector2(Input.GetAxisRaw(Horizontal), Input.GetAxisRaw(Vertical));
+
+        public void RatMovement()
+            => characterData.ratRigidbody2D.velocity =
+                new Vector2(
+                    movementDirection.normalized.x * characterData.speed,
+                    movementDirection.normalized.y * characterData.speed);
+
+
+        public void RatMovementOrStopped()
+        {
+            if (!characterData.isStopped)
+                RatMovement();
+            else
+                characterData.ratRigidbody2D.velocity = Vector2.zero;
+        }
+    }
+}
