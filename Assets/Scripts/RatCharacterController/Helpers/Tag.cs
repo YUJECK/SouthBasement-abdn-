21cd4e1e16bc1c2670ee790dd1484e9b484777e7
RimuruDev <Rimuru.Dev@gmail.com>
2022-05-17 18:35:27 +0400

    Added Tag helper.
    Stores all possible tags. To reduce the chance of a typo.

---
+++ Assets/Scripts/RatCharacterController/Helpers/Tag.cs
@@ -0,0 +1,19 @@
+﻿namespace RimuruDev.Helpers
+{
+    public struct Tag
+    {
+        // Tags
+        public const string Player = "Player";
+
+        // Input
+        public const string Horizontal = "Horizontal";
+        public const string Vertical = "Vertical";
+
+        // Level
+        public const string StartPoint = "StartPoint";
+
+        // Animations
+        public const string Is_Run = "Is_Run";
+        public const string Is_Sprint = "Is_Sprint";
+    }
+}
