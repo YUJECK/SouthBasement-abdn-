7fa3ab7180e45c6570e7694e32032afc54299a7d
YUJECK <kolajkuceravkin@gmail.com>
2022-05-31 07:36:25 +0200

    Доработал спрайт коробки и починил спавн

--- Assets/Scripts/Inventory&Items/ItemsScrObj/MelleRangeWeapon.cs
+++ Assets/Scripts/Inventory&Items/ItemsScrObj/MelleRangeWeapon.cs
@@ -1,5 +1,12 @@
 using UnityEngine;
 
+
+public enum TypeOfAttack
+{
+    Pinpoint,
+    Wide,
+    Above
+}
 [CreateAssetMenu(fileName = "New MelleWeapon", menuName = "Items/MelleWeapons")]
 public class MelleRangeWeapon : ScriptableObject
 {
@@ -9,6 +16,7 @@ public class MelleRangeWeapon : ScriptableObject
     public EffectsList effect;
     public float effectTime;
     public int cost;
+    public TypeOfAttack typeOfAttack;
     [Tooltip("Attack Range")] public float attackRange;
     [Tooltip("Speed of attack")] public float attackRate;
     public float lenght; //Длина атаки
@@ -16,7 +24,7 @@ public class MelleRangeWeapon : ScriptableObject
     public float stunTime = 0f;
     float nextTime = 0f;
     public int chanceOfDrop;
-    
+
     //Другие переменные
     public Sprite sprite;
     public Sprite spriteInGame;
