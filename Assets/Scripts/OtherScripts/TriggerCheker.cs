07d80bd472cf2632664c1ee0930c0723cb02df9a
YUJECK <kolajkuceravkin@gmail.com>
2022-06-29 07:10:05 +0300

    Баг пофикшен

--- Assets/Scripts/OtherScripts/TriggerCheker.cs
+++ Assets/Scripts/OtherScripts/TriggerCheker.cs
@@ -4,54 +4,77 @@ using UnityEngine.Events;
 
 public class TriggerCheker : MonoBehaviour
 {
+    [Header("Настройки")]
     [SerializeField] private ChangeMode changeMode = ChangeMode.Trigger;
     [SerializeField] private TriggerMode triggerMode = TriggerMode.OnlyAsTag;
-
-    public UnityEvent<GameObject> onEnter = new UnityEvent<GameObject>();
-    public UnityEvent<GameObject> onExit = new UnityEvent<GameObject>();
-    public GameObject nowEnter;
-    public GameObject nowExit;
-    public bool trigger;
     public string targetTag = "Player";
-    public Transform obj = null;
-    public List<Transform> other = new List<Transform>();
+
+    [Header("События")]
+    public UnityEvent<GameObject> onEnter = new UnityEvent<GameObject>();
+    public UnityEvent<GameObject> onStay = new UnityEvent<GameObject>();
+    public UnityEvent<GameObject> onExit = new UnityEvent<GameObject>();
+    [Header("Объекты")]
+    public GameObject nowEnter;
+    public GameObject nowStay;
+    public GameObject nowExit;
+    public Transform triggerObject = null;
+    public List<Transform> otherObjects = new List<Transform>();
+    [HideInInspector] public bool trigger;
 
     private void Update()
     {
-        if (changeMode == ChangeMode.Trigger && trigger && obj == null) trigger = false;
+        if (changeMode == ChangeMode.Trigger && trigger && triggerObject == null) trigger = false;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {   
-        nowEnter = other.gameObject;
-        trigger = true;
-
         if (changeMode == ChangeMode.Trigger && triggerMode == TriggerMode.OnlyAsTag && other.CompareTag(targetTag))
         {
+            trigger = true;
+            nowEnter = other.gameObject;
             onEnter.Invoke(nowEnter);
-            obj = other.transform;
+            triggerObject = other.transform;
         }
         else if (changeMode == ChangeMode.Trigger && triggerMode == TriggerMode.All)
         {
+            trigger = true;
+            nowEnter = other.gameObject;
             onEnter.Invoke(nowEnter);
-            this.other.Add(nowEnter.transform);
+            this.otherObjects.Add(nowEnter.transform);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {   
+        if (changeMode == ChangeMode.Trigger && triggerMode == TriggerMode.OnlyAsTag && other.CompareTag(targetTag))
+        {
+            trigger = true;
+            onStay.Invoke(nowStay);
+            nowStay = other.gameObject;
+            triggerObject = other.transform;
+        }
+        else if (changeMode == ChangeMode.Trigger && triggerMode == TriggerMode.All)
+        {
+            trigger = true;
+            onStay.Invoke(nowStay);
+            nowStay = other.gameObject;
+            this.otherObjects.Add(nowEnter.transform);
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        nowExit = other.gameObject;
-        onExit.Invoke(nowExit);
-        trigger = false;
-
         if (changeMode == ChangeMode.Trigger && triggerMode == TriggerMode.OnlyAsTag && other.CompareTag(targetTag))
         {
+            trigger = false;
+            nowExit = other.gameObject;
             onExit.Invoke(nowExit);
-            obj = null;
+            triggerObject = null;
         }
         else if (changeMode == ChangeMode.Trigger && triggerMode == TriggerMode.All)
         {
+            trigger = false;
+            nowExit = other.gameObject;
             onExit.Invoke(nowExit);
-            this.other.Remove(nowExit.transform);
+            this.otherObjects.Remove(nowExit.transform);
         }
     }
     
