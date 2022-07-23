5e217c0c9e6be6e3424ce0d203a36ed05f45ec95
YUJECK <kolajkuceravkin@gmail.com>
2022-07-23 10:48:03 +0300

    Начал обновлять генерацию, пока были ток тесты

--- Assets/Scripts/Generator/RoomsNPCList.cs
+++ Assets/Scripts/Generator/RoomsNPCList.cs
@@ -20,30 +20,34 @@ public class RoomsNPCList : MonoBehaviour
     }
     private void StartSpawning()
     {          
-        //Находим и добавляем все комнаты для нпс в лист
-        for(int i = 0; i < generationManager.NowSpawnedRooms; i++)
+        if(generationManager.NPCsRoomsCount != 0)
         {
-            if(generationManager.roomsList[i].spawnerNPC != null)
-                NPCRooms.Add(generationManager.roomsList[i].spawnerNPC);
-        }
+            //Находим и добавляем все комнаты для нпс в лист
+            for(int i = 0; i < generationManager.NowSpawnedRooms; i++)
+            {
+                if(generationManager.roomsList[i].spawnerNPC != null)
+                    NPCRooms.Add(generationManager.roomsList[i].spawnerNPC);
+            }
 
-        //Спавним коробку и торговца
-        if(NPCRooms[generationManager.BoxIndex-1] != null)
-        {
-            NPCRooms[generationManager.BoxIndex-1].SpawnNPC(Box);
-            NPCRooms.Remove(NPCRooms[generationManager.BoxIndex-1]);    
-        }
-        if(IsTraderSpawning && NPCRooms[generationManager.TraderIndex-1] != null)
-        {
-            NPCRooms[generationManager.TraderIndex-1].SpawnNPC(Trader);
-            NPCRooms.Remove(NPCRooms[generationManager.TraderIndex-1]);
-        }
+            //Спавним коробку и торговца
+            if(NPCRooms[generationManager.BoxIndex-1] != null)
+            {
+                NPCRooms[generationManager.BoxIndex-1].SpawnNPC(Box);
+                NPCRooms.Remove(NPCRooms[generationManager.BoxIndex-1]);    
+            }
+            if(IsTraderSpawning && NPCRooms[generationManager.TraderIndex-1] != null)
+            {
+                NPCRooms[generationManager.TraderIndex-1].SpawnNPC(Trader);
+                NPCRooms.Remove(NPCRooms[generationManager.TraderIndex-1]);
+            }
         
-        //YUJECK: Если нпс будет много то наверное лучше будет поменять на NPCRooms.Count
-        for(int i = 0; i < NPCRooms.Count; i++)
-        { 
-            if(i >= NPCs.Count) break; //Делаем бряк если i вышло за количество НПС
-            else if(NPCRooms[i] != null) NPCRooms[i].SpawnNPC(); //Спавним НПС в той комнате
+            //YUJECK: Если нпс будет много то наверное лучше будет поменять на NPCRooms.Count
+            for(int i = 0; i < NPCRooms.Count; i++)
+            { 
+                if(i >= NPCs.Count) break; //Делаем бряк если i вышло за количество НПС
+                else if(NPCRooms[i] != null) NPCRooms[i].SpawnNPC(); //Спавним НПС в той комнате
+            }
+
         }
     }
 }
