f86f679b70d471738b248ce99b6aacc7c690a94c
YUJECK <kolajkuceravkin@gmail.com>
2022-09-15 11:17:58 +0300

    Добавлен поиск в ширину

--- Assets/Scripts/Basics/CreaturesComponents/GridManager.cs
+++ Assets/Scripts/Basics/CreaturesComponents/GridManager.cs
@@ -15,16 +15,63 @@ namespace CreaturesAI.Pathfinding
         [SerializeField] private int gridWidth = 300;
         [SerializeField] private int gridHeight = 300;
 
-        private int[,] grid = { };
         [SerializeField] private ObstacleDefining obstacleDefining;
         [SerializeField] private List<string> tagBlackList = new List<string>();
         [SerializeField] private List<string> obstacleTags = new List<string>();
+        
+        private int[,] grid = { };
+        private Dictionary<Vector2Int, int> editedPoints = new Dictionary<Vector2Int, int>();
 
         //getters
-        public int GetPoint(Vector2 point) => grid[(int)point.x, (int)point.y];
+        public int GetPoint(Vector2Int point)
+        {
+            if (CheckRange(point))
+                return grid[(int)point.x, (int)point.y];
+            else return grid[0, 0];
+        }
+        public int GetEditedPoint(Vector2Int point)
+        {
+            if (editedPoints.ContainsKey(point))
+                return editedPoints[point];
+            else
+            {
+                Debug.LogWarning("Point " + point + " hasn't been edited");
+                return 0;
+            }
+        }
         public int GridWidth => gridWidth;
         public int GridHeight => gridHeight;
+        
+        //setters
+        public void EditPoint(Vector2Int point, int newValue)
+        {
+            if(CheckRange(point))
+            {
+                editedPoints.Add(point, grid[point.x, point.y]);
+                grid[point.x, point.y] = newValue;
+            }
+        }
+        public void DiscardPointEditing(Vector2Int point)
+        {
+            if (editedPoints.ContainsKey(point))
+            {
+                grid[point.x, point.y] = editedPoints[point];
+                editedPoints.Remove(point);
+            }
+            else Debug.LogWarning("Point " + point + " hasn't been edited");
+        }
 
+        //other
+        public bool CheckRange(Vector2Int point)
+        {
+            if ((point.x > 0 && point.x < GridWidth) && (point.y > 0 && point.y < GridHeight))
+                return true;
+            else
+            {
+                Debug.LogWarning("Point" + point + " is out of range");
+                return false;
+            }
+        }
         private void GenerateGrid()
         {
             grid = new int[gridWidth, gridHeight];
@@ -39,6 +86,7 @@ namespace CreaturesAI.Pathfinding
                     {
                         if (nextHit.collider != null && !nextHit.collider.isTrigger)
                         {
+                            Debug.Log("Collider");
                             if(obstacleDefining == ObstacleDefining.AllColliders) grid[x, y] = 1;
                             else if(obstacleTags.Contains(nextHit.transform.tag)) grid[x, y] = 1;
                         }
--- Assets/Scripts/Basics/CreaturesComponents/Pathfinder.cs
+++ Assets/Scripts/Basics/CreaturesComponents/Pathfinder.cs
@@ -6,65 +6,108 @@ namespace CreaturesAI.Pathfinding
 {
     public class Pathfinder : MonoBehaviour
     {
+        private class Point
+        {
+            private int x;
+            private int y;
+
+            private Point cameFrom;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public void SetCameFrom(Point startingPoint) => cameFrom = startingPoint;
+            public Point GetCameFrom() => cameFrom;
+            public int X => x;
+            public int Y => y;
+            public void SetXY(Vector2 point)
+            {
+                x = (int)point.x;
+                y = (int)point.y;
+            }
+        }
+
         [SerializeField] List<Vector2> test;
         
         GridManager grid;
         [SerializeField] GameObject obj;
-        public List<Vector2Int> FindPath(Vector2Int startPoint, Vector2Int endPoint)
+        public List<Vector2> FindPath(Vector2Int startPoint, Vector2Int endPoint)
         {
-            Queue<Vector2Int> nextPoints = new Queue<Vector2Int>();
+            Queue<Point> nextPoints = new Queue<Point>();
             bool[,] visitedPoints = new bool[grid.GridWidth, grid.GridHeight];
+            Point start = new Point(startPoint.x, startPoint.y);
 
-            nextPoints.Enqueue(startPoint);
+            nextPoints.Enqueue(start);
 
             while (nextPoints.Count > 0)
             {
-                if (nextPoints.Peek().x == endPoint.x && nextPoints.Peek().y == endPoint.y)
-                    return new List<Vector2Int>();
-                visitedPoints[(int)nextPoints.Peek().x, (int)nextPoints.Peek().y] = true;
+                if (nextPoints.Peek().X == endPoint.x && nextPoints.Peek().Y == endPoint.y)
+                    return RestorePath(nextPoints.Peek());
+                visitedPoints[(int)nextPoints.Peek().X, (int)nextPoints.Peek().Y] = true;
 
-                List<Vector2Int> neibhourPoints = GetNeibhourPoints(nextPoints.Peek(), visitedPoints);
+                List<Point> neibhourPoints = GetNeibhourPoints(nextPoints.Peek(), visitedPoints);
 
-                foreach (Vector2Int point in neibhourPoints)
+                foreach (Point point in neibhourPoints)
                 {
+                    point.SetCameFrom(nextPoints.Peek());
                     nextPoints.Enqueue(point);
-                    visitedPoints[point.x, point.y] = true;
-                    Instantiate(obj, (Vector2)point, Quaternion.identity);
+                    visitedPoints[point.X, point.Y] = true;
                 }
                 nextPoints.Dequeue();
             }
 
-            return new List<Vector2Int>();
+            return new List<Vector2>();
         }
-        private List<Vector2Int> GetNeibhourPoints(Vector2Int point, bool[,] ignoredPoints)
+        private List<Point> GetNeibhourPoints(Point point, bool[,] ignoredPoints)
         {
-            List<Vector2Int> neibhourPoints = new List<Vector2Int>();
-            List<Vector2Int> pointsToCheck = new List<Vector2Int>();
+            List<Point> neibhourPoints = new List<Point>();
+            List<Point> pointsToCheck = new List<Point>();
             //adding points to check
-            pointsToCheck.Add(new Vector2Int(point.x + 1, point.y + 1));
-            pointsToCheck.Add(new Vector2Int(point.x + 1, point.y - 1));
-            pointsToCheck.Add(new Vector2Int(point.x - 1, point.y + 1));
-            pointsToCheck.Add(new Vector2Int(point.x, point.y + 1));
-            pointsToCheck.Add(new Vector2Int(point.x + 1, point.y));
+            pointsToCheck.Add(new Point(point.X, point.Y + 1));
+            pointsToCheck.Add(new Point(point.X, point.Y - 1));
+            pointsToCheck.Add(new Point(point.X + 1, point.Y));
+            pointsToCheck.Add(new Point(point.X - 1, point.Y));
+            pointsToCheck.Add(new Point(point.X + 1, point.Y + 1));
+            pointsToCheck.Add(new Point(point.X - 1, point.Y - 1));
+            pointsToCheck.Add(new Point(point.X + 1, point.Y - 1));
+            pointsToCheck.Add(new Point(point.X - 1, point.Y + 1));
             //cheking points
-            foreach (Vector2Int nextPoint in pointsToCheck)
+            foreach (Point nextPoint in pointsToCheck)
             {
                 if (CheckPoint(nextPoint, ignoredPoints))
                     neibhourPoints.Add(nextPoint);
             }
             return neibhourPoints;
         }
-        private bool CheckPoint(Vector2Int point, bool[,] ignoredPoints)
+        private bool CheckPoint(Point point, bool[,] ignoredPoints)
         {
-            if (grid.GetPoint(point) == 0 && !ignoredPoints[point.x, point.y])
+            if (grid.GetPoint(new Vector2Int(point.X, point.Y)) == 0 && !ignoredPoints[point.X, point.Y])
                 return true;
             else return false;
         }
+        private List<Vector2> RestorePath(Point endPoint)
+        {
+            Point current = endPoint;
+            List<Vector2> path = new List<Vector2>();
+
+            do
+            {
+                Instantiate(obj, new Vector2(current.X, current.Y), Quaternion.identity);
+                path.Add(new Vector2(current.X, current.Y));
+                current = current.GetCameFrom();
+            } while ((current.GetCameFrom() != null));
+
+            return path;
+        }
+        
         void Start()
         {
             grid = FindObjectOfType<GridManager>();
             //test = GetNeibhourPoints(new Vector2(10, 10), new List<Vector2>());
-            FindPath(new Vector2Int(40, 40), new Vector2Int(60, 60));
+            test = FindPath(new Vector2Int(40, 40), new Vector2Int(60, 60));
         }
     }
 }
