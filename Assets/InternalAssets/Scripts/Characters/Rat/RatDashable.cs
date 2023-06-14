9d6a462bc52fbee1889f73890ab735e96fa440d3
YUJECK <76834558+YUJECK@users.noreply.github.com>
2023-06-14 10:28:16 +0300

    Чуть причесал код рывка

--- Assets/InternalAssets/Scripts/Characters/Rat/RatDashable.cs
+++ Assets/InternalAssets/Scripts/Characters/Rat/RatDashable.cs
@@ -1,26 +1,25 @@
-﻿using System;
-using System.Collections;
-using Cysharp.Threading.Tasks;
+﻿using System.Collections;
 using TheRat.InputServices;
 using TheRat.Player;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace TheRat.Characters.Rat
 {
     public sealed class RatDashable : IDashable
     {
-        private IMovable _movable;
-        private Transform _transform;
+        private readonly IMovable _movable;
+        private readonly Transform _transform;
         
-        private MonoBehaviour _coroutineRunner;
+        private readonly MonoBehaviour _coroutineRunner;
         private readonly PlayerAnimator _playerAnimator;
+        private readonly Rigidbody2D _rigidbody2D;
         private bool _blocked;
 
         public RatDashable(IInputService inputService, IMovable movable, Transform transform, PlayerAnimator playerAnimator, MonoBehaviour coroutineRunner)
         {
             _movable = movable;
             _transform = transform;
+            _rigidbody2D = transform.GetComponent<Rigidbody2D>();
             _coroutineRunner = coroutineRunner;
             _playerAnimator = playerAnimator;
             
@@ -37,33 +36,37 @@ namespace TheRat.Characters.Rat
 
         private IEnumerator DashCoroutine()
         {
-            _blocked = true;
-            _movable.CanMove = false;
+            StartDash();
             {
-                _playerAnimator.PlayDash();
-                _transform.gameObject.layer = 11;
                 var dashMove = GetPositionInVector2() + _movable.Movement;
                 
-                float dashStopTime = Time.time + 0.2f;
+                var dashStopTime = Time.time + 0.2f;
                 
                 while (Time.time < dashStopTime)
                 {
-                    _transform.GetComponent<Rigidbody2D>().MovePosition(Vector2.MoveTowards(_transform.position, dashMove, Time.deltaTime * 40));
+                    _rigidbody2D.MovePosition(Vector2.MoveTowards(_transform.position, dashMove, Time.deltaTime * 40));
                     yield return new WaitForFixedUpdate();
                 }
-                
-                _transform.gameObject.layer = 7;
             }
-            yield return new WaitForSeconds(0.1f);
-            _movable.CanMove = true;
-            
+            ReleaseDash();
+
             yield return new WaitForSeconds(0.7f);
             _blocked = false;
         }
-        
-        private Vector2 GetPositionInVector2()
+
+        private void StartDash()
         {
-            return new Vector2(_transform.position.x, _transform.position.y);
+            _movable.CanMove = false;
+            _playerAnimator.PlayDash();
+            _transform.gameObject.layer = 11;
         }
+
+        private void ReleaseDash()
+        {
+            _transform.gameObject.layer = 7;
+            _movable.CanMove = true;
+        }
+
+        private Vector2 GetPositionInVector2() => new(_transform.position.x, _transform.position.y);
     }
 }   
