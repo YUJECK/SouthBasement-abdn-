3613ba2613a8fc1f4f1f10ae4205f1accd467c53
YUJECK <76834558+YUJECK@users.noreply.github.com>
2023-07-05 01:16:09 +0300

    Добавил чуть звуков

--- Assets/InternalAssets/Locations/Basement/Enemies/ArmouredRat/AI/ArmouredRatHealth.cs
+++ Assets/InternalAssets/Locations/Basement/Enemies/ArmouredRat/AI/ArmouredRatHealth.cs
@@ -1,15 +1,18 @@
 ﻿using SouthBasement.AI;
+using UnityEngine;
 
 namespace SouthBasement.Basement.Enemies.ArmouredRat.AI
 {
     public sealed class ArmouredRatHealth : DefaultEnemyHealth
     {
+        [SerializeField] private AudioSource _shieldHitSound;
         public bool CurrentDefends { get; set; }
 
         public override void Damage(int damage, string[] args)
         {
             if(!CurrentDefends)
                 base.Damage(damage, args);
+            else _shieldHitSound.Play();
         }
     }
 }
