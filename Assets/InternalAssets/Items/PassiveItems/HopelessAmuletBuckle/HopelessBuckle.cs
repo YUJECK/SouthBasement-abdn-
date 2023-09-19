213abc465291baf361ca12c427f7dec013f8a79e
YUJECK <76834558+YUJECK@users.noreply.github.com>
2023-09-20 00:24:37 +0300

    Добавил безысходную бляшку

---
+++ Assets/InternalAssets/Items/PassiveItems/HopelessAmuletBuckle/HopelessBuckle.cs
@@ -0,0 +1,19 @@
+﻿using SouthBasement.InventorySystem.ItemBase;
+using SouthBasement.Items;
+using SouthBasement.Items.Weapons;
+using UnityEngine;
+
+namespace SouthBasement.BaseScripts.Tests.HopelessAmulet
+{
+    [CreateAssetMenu(menuName = AssetMenuHelper.PassiveItem + nameof(HopelessBuckle))]
+    public sealed class HopelessBuckle : PassiveItem
+    {
+        [SerializeField] private float bonus;
+
+        public override void OnAddedToInventory()
+            => WeaponsStatsMultiplier.GetMultiplier(AttackTags.Magic).Damage += bonus;
+        
+        public override void OnRemovedFromInventory()
+            => WeaponsStatsMultiplier.GetMultiplier(AttackTags.Magic).Damage -= bonus;
+    }
+}
